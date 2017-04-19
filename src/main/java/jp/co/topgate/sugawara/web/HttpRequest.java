4fa42c0f2bba7c2e9cd248b3ff9c010845244a79
sakura818 <haruka.sugawara@topgate.co.jp>
2017-04-19 16:19:03 +0900

    request parse

--- src/main/java/jp/co/topgate/sugawara/web/HttpRequest.java
+++ src/main/java/jp/co/topgate/sugawara/web/HttpRequest.java
@@ -12,6 +12,7 @@ import java.util.List;
 public class HttpRequest {
 
     private BufferedReader bufferedReader;
+    private String requestLine;
     private String method;
     private String requestUri;
     private String httpVersion;
@@ -19,24 +20,48 @@ public class HttpRequest {
     private String requestMessageBody;
 
 
+    String setHTTPRequest(InputStream inputStream) throws IOException {
+
+        /*
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            this.requestLine = bufferedReader.readLine();
+            System.out.println("requestLineを出力します" + requestLine);
+        } catch (IOException e) {
+            System.out.println("requestLineの出力に失敗しました");
+        }
+
+        return requestLine;
+        */
+
+    }
+
+
 
     public String getMethod() {
-        return ("GET");
+        method = requestLine.split("");//リクエストの最初の文字から１つめの空白の一文字手前まで
+        return this.getMethod;
     }
 
     public String getRequestUri() {
+        requestUri = requestLine.split("");//リクエストの１つめの空白の次の文字から２つめの空白の一文字手前まで
         return this.requestUri;
     }
 
-    public String getHttpVersion() {
+    public String getHttpVersion()
+    {
+        httpVersion = requestLine.split("");//リクエストの２つめの空白の次の文字からCRLFまで
         return this.httpVersion;
     }
 
+
     public String getHeader() {
+        requestHeader =  bufferedReader.split();//リクエストの１つめのCRLFから２つめのCRLFまで
         return this.requestHeader;
     }
 
     public String getMessageBody() {
+        requestMessageBody =  bufferedReader.split();//リクエストの３つめのCRLFから文字列の末尾まで
         return this.requestMessageBody;
     }
 
