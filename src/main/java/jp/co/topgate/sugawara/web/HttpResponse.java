d66559a35ae592b53fc94402c70a88198d8d5585
sakura818 <haruka.sugawara@topgate.co.jp>
2017-04-28 22:41:04 +0900

    Add generalHeader

--- src/main/java/jp/co/topgate/sugawara/web/HttpResponse.java
+++ src/main/java/jp/co/topgate/sugawara/web/HttpResponse.java
@@ -5,8 +5,8 @@ import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.HashMap;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * Created by haruka.sugawara on 2017/04/13.
@@ -161,11 +161,23 @@ public class HttpResponse {
         if (CONTENT_TYPE.containsKey(extension)) {
             System.out.print("extensionのContent-Typeは");
             System.out.println(CONTENT_TYPE.get(extension));
-            return "ContentType=" + CONTENT_TYPE.get(extension);
+            return "ContentType: " + CONTENT_TYPE.get(extension);
         } else {
             return "指定したCONTENT_TYPEのキーは存在しません";
         }
 
+    }
+
+    /*
+    HTTP-dateを生成する
+     */
+    public String getHttpDateTime() {
+        SimpleDateFormat rfc1123DateFormat =
+                new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss zzz", java.util.Locale.US);
+        rfc1123DateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        Date httpDateTime = Calendar.getInstance().getTime();
+        return rfc1123DateFormat.format(httpDateTime);
     }
 
 
@@ -176,9 +188,15 @@ public class HttpResponse {
     public String generateGeneralHeader() {
 
         StringBuilder sb = new StringBuilder();
-        sb.append("hoge" + "¥n");
-        sb.append("hoge" + "¥n");
-        sb.append("hoge" + "¥n");
+        //sb.append("Cache-Control: " + "¥n");// cacheに関することなので今回は使用しない
+        //sb.append("Connection: " + "¥n");// 永続的接続Keep-AliveがHTTP/1.1ではデフォルトである。これを無効にしたいときにConnection: closeとする　今回は使用しない
+        sb.append("Date: " + getHttpDateTime() + "¥n");// メッセージが生成された日付・時刻を表す。ィールド値は、section 3.3.1 にて示される HTTP-date であり、RFC 1123 [8] の時刻フォーマット
+        //sb.append("Pragma: " + "¥n");// cacheに関することなので今回は使用しない
+        //sb.append("Trailer: " + "¥n");// チャンク形式で今回は送らないので無視する
+        //sb.append("Transfer-Encoding: " + "¥n");// 転送エンコーディングchunkなどを今回は使用しないので無視する
+        //sb.append("Upgrade: " + "¥n");// Upgrade 一般ヘッダは、クライアントが他にどんな通信プロトコルをサポートするかを表し、サーバがプロトコルを切り換えた方がいいと判断した場合に使わせたいものを指定させる。ex:Upgrade: HTTP/2.0, SHTTP/1.3 今回はHTTP/1.1しか想定していないため無視する
+        //sb.append("Via: " + "¥n");// proxyに関することなので今回は無視する
+        //sb.append("Warning: " + "¥n");// キャッシュの処理やメッセージのエンティティボディに適用される変化から意味的透過性が欠けている可能性がある事を警告する。今回は無視する
 
         String generalHeader = new String(sb);
         return generalHeader;
@@ -192,7 +210,10 @@ public class HttpResponse {
     public String generateResponseHeader() {
 
         StringBuilder sb = new StringBuilder();
+        sb.append("Accept-Ranges:" + "none" + "¥n");// リソースへのいかなる範囲リクエストも受け入れない
+        //sb.append("Age: " + "age-value");// Age レスポンスヘッダは、オリジンサーバがレスポンスを生成した (あるいは再検証した) 時点からの送信者の推定経過時間を示す。キャッシュエントリが新鮮かどうかを知るために、キャッシュは経過時間が その有効期間を超えているかどうかを知る必要がある。
         sb.append("hoge" + "¥n");
+        sb.append("Server: " + "sakura818uuu" + "¥n");// Serverレスポンスヘッダフィールドは、リクエストを処理するオリジンサーバが使っているソフトウェアについての情報を含んでいる。ex:Server: Apache[Ver]
         sb.append("hoge" + "¥n");
         sb.append("hoge" + "¥n");
 
