81867289c97121758a4eba689e191494fc7a8511
sakura818 <haruka.sugawara@topgate.co.jp>
2017-04-28 14:31:16 +0900

    wip

--- src/main/java/jp/co/topgate/sugawara/web/HttpRequest.java
+++ src/main/java/jp/co/topgate/sugawara/web/HttpRequest.java
@@ -28,38 +28,6 @@ public class HttpRequest {
     HttpServer httpServer = new HttpServer();
     BufferedReader reqdata = httpServer.getReqdata();// rename
 
-
-    /*
-    改行文字を区切り文字としてリクエストを分割する
-     */
-
-    public String[] reqDataDivide() throws IOException {
-        reqdata.readLine();// 文字ストリームから改行終端文字を読み取るのがreadLineメソッド
-        String reqDelimiterDivide[] = reqdata.toString().split("\\r?\\n");// mac,windows crlf　
-        //reqDelimiterDivide= new String[];
-
-        this.requestLine = reqDelimiterDivide[0];//　1つめの改行終端文字までを変数reqheaderにいれる処理
-        this.requestHeader = reqDelimiterDivide[1];//　2つめの改行終端文字までを変数reqheaderにいれる処理
-        this.emptyLine = reqDelimiterDivide[2];//　3つめの改行終端文字までを変数emptylineにいれる処理
-        this.requestMessageBody = reqDelimiterDivide[3];//　4つめの改行終端文字までを変数reqmessagebodyにいれる処理
-
-        return reqDelimiterDivide;
-    }
-
-    /*
-    空白文字を区切り文字としてリクエストuriを分割する
-     */
-
-    public String[] requestLineDivide() {
-        String reqUriDelimiterDivide[] = requestLine.split("\\s");
-
-        this.method = reqUriDelimiterDivide[0];//　1つめの空白文字までを変数reqlineにいれる処理
-        this.requestUri = reqUriDelimiterDivide[1];//　2つめの空白文字までを変数reqlineにいれる処理
-        this.httpVersion = reqUriDelimiterDivide[2];//　3つめの空白文字までを変数reqlineにいれる処理
-        return reqUriDelimiterDivide;
-
-    }
-
     /*
     methodの処理 getter
      */
@@ -116,13 +84,52 @@ public class HttpRequest {
         return this.decodeUri;
     }
 
+    /*
+   PathUriの処理 getter
+     */
+
+    public String getPathUri() {
+        return this.pathUri;
+    }
+
+
+    /*
+    改行文字を区切り文字としてリクエストを分割する
+     */
+
+    public String[] reqDataDivide() throws IOException {
+        reqdata.readLine();// 文字ストリームから改行終端文字を読み取るのがreadLineメソッド
+        String reqDelimiterDivide[] = reqdata.toString().split("\\r?\\n");// mac,windows crlf　
+        //reqDelimiterDivide= new String[];
+
+        this.requestLine = reqDelimiterDivide[0];//　1つめの改行終端文字までを変数reqheaderにいれる処理
+        this.requestHeader = reqDelimiterDivide[1];//　2つめの改行終端文字までを変数reqheaderにいれる処理
+        this.emptyLine = reqDelimiterDivide[2];//　3つめの改行終端文字までを変数emptylineにいれる処理
+        this.requestMessageBody = reqDelimiterDivide[3];//　4つめの改行終端文字までを変数reqmessagebodyにいれる処理
+
+        return reqDelimiterDivide;
+    }
+
+    /*
+    空白文字を区切り文字としてリクエストuriを分割する
+     */
+
+    public String[] requestLineDivide() {
+        String reqUriDelimiterDivide[] = requestLine.split("\\s");
+
+        this.method = reqUriDelimiterDivide[0];//　1つめの空白文字までを変数reqlineにいれる処理
+        this.requestUri = reqUriDelimiterDivide[1];//　2つめの空白文字までを変数reqlineにいれる処理
+        this.httpVersion = reqUriDelimiterDivide[2];//　3つめの空白文字までを変数reqlineにいれる処理
+        return reqUriDelimiterDivide;
+
+    }
 
 
     /*
     もし、Request-URI に "% HEX HEX" エンコード [42] が使用されていたら、オリジンサーバはそのリクエストを適切に解釈するためにその
     Request-URI をデコードし*なければならない*。
      */
-    public String requestUriDecode() throws UnsupportedEncodingException{
+    public String requestUriDecode() throws UnsupportedEncodingException {
         String decodeUri = URLDecoder.decode(this.requestUri, "UTF-8");
         return decodeUri;
     }
@@ -132,15 +139,14 @@ public class HttpRequest {
      */
     public String requestUriPath() {
         //URI u = new URI(this.decodeUri);
-        String pathUri = decodeUri.getPath();//?
+        String pathUri = this.decodeUri.getPath();//?
         return pathUri;
     }
-    
+
 
     //リクエストURIから拡張子を取得する　
     //lastIndexOfでnullが来た場合の処理ができていない　nullPointerExceptionになる
-    public String convertPathUriToExtension(String requestUri) {
-        //String fileExtension = new String();
+    public String convertPathUriToExtension() {
         int lastDotPosition = this.pathUri.lastIndexOf(".");
         if (lastDotPosition != -1) {
             return this.requestUri.substring(lastDotPosition + 1);
