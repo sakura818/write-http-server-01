44fe182f6a7fbe14e6fd792e62458325c31733cf
sakura818 <haruka.sugawara@topgate.co.jp>
2017-05-11 17:56:29 +0900

    Move status code test to response line test

--- src/test/java/jp/co/topgate/sugawara/web/HttpResponseStatusLineContentTest.java
+++ src/test/java/jp/co/topgate/sugawara/web/HttpResponseStatusLineContentTest.java
@@ -1,7 +1,54 @@
 package jp.co.topgate.sugawara.web;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+
 /**
- * Created by haruka.sugawara on 2017/05/11.
+ * HttpResponseStatusLineContentTest Class
+ * リクエストに応じて適切なステータスコードを判断するのをテストするクラス
+ * @author sakura818
  */
+
+
 public class HttpResponseStatusLineContentTest {
+    @Test
+    public void StatusCodeを200にしたときのテスト() {
+        StatusCode status = new StatusCode();
+
+        status.setStatusCode(200);
+        assertThat(200, is(status.getStatusCode()));
+        assertThat("OK", is(status.getReasonPhrase()));
+        assertThat("200 OK", is(status.getStatusCodeAndReasonPhrase()));
+    }
+
+    @Test
+    public void StatusCodeを400にしたときのテスト() {
+        StatusCode status = new StatusCode();
+
+        status.setStatusCode(400);
+        assertThat(400, is(status.getStatusCode()));
+        assertThat("Bad Request", is(status.getReasonPhrase()));
+        assertThat("400 Bad Request", is(status.getStatusCodeAndReasonPhrase()));
+    }
+
+    @Test
+    public void StatusCodeを404にしたときのテスト() {
+        StatusCode status = new StatusCode();
+
+        status.setStatusCode(404);
+        assertThat(404, is(status.getStatusCode()));
+        assertThat("Not Found", is(status.getReasonPhrase()));
+        assertThat("404 Not Found", is(status.getStatusCodeAndReasonPhrase()));
+    }
+
+    @Test
+    public void 想定していないStatusCodeのときのテスト() {
+        StatusCode status = new StatusCode();
+
+        status.setStatusCode(500);
+        assertThat(500, is(status.getStatusCode()));
+        assertThat("Unknown", is(status.getReasonPhrase()));
+        assertThat("500 Unknown", is(status.getStatusCodeAndReasonPhrase()));
+    }
 }
