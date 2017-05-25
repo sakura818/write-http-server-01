d777241eb4c9994610bc4f7f05aa966a47007c54
sakura818 <haruka.sugawara@topgate.co.jp>
2017-05-25 17:33:17 +0900

    Fix response body

--- src/main/java/jp/co/topgate/sugawara/web/HttpResponseMessageBodyBuilder.java
+++ src/main/java/jp/co/topgate/sugawara/web/HttpResponseMessageBodyBuilder.java
@@ -13,39 +13,26 @@ import java.io.*;
 public class HttpResponseMessageBodyBuilder {
 
     private File filePath;
-    private int statusCode;
 
-    public HttpResponseMessageBodyBuilder(File filePath, int statusCode) {
+    public HttpResponseMessageBodyBuilder(File filePath) {
         this.filePath = filePath;
-        this.statusCode = statusCode;
     }
 
     /**
-     * statusCodeで条件分岐した後、filePathもしくはmessageBodyHtmlをバイト型の配列で読み込みHttpResponseMessageBodyを生成する
+     * filePathをバイト型の配列で読み込みHttpResponseMessageBodyを生成する
      *
      * @return messageBody
      */
 
     public byte[] build() throws IOException {
         byte[] messageBody = new byte[(int) this.filePath.length()];
-        if (this.statusCode == 200) {
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(this.filePath));
-            try {
-                bufferedInputStream.read(messageBody);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            } finally {
-                bufferedInputStream.close();
-            }
-        } else if (this.statusCode == 404) {
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(this.filePath));
-            try {
-                bufferedInputStream.read(messageBody);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            } finally {
-                bufferedInputStream.close();
-            }
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(this.filePath));
+        try {
+            bufferedInputStream.read(messageBody);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            bufferedInputStream.close();
         }
         return messageBody;
     }
