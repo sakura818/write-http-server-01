8ca2f2a9044438ae9258e69247e283677cb56ccf
sakura818 <haruka.sugawara@topgate.co.jp>
2017-05-30 14:23:28 +0900

    Add public static class extractExtensionメソッドのテスト

--- src/test/java/jp/co/topgate/sugawara/web/HttpResponseMessageHeaderBuilderTest.java
+++ src/test/java/jp/co/topgate/sugawara/web/HttpResponseMessageHeaderBuilderTest.java
@@ -90,11 +90,16 @@ public class HttpResponseMessageHeaderBuilderTest {
         assertThat((httpResponseMessageHeaderBuilder.catchContentType(new File("hoge.hoge"))), is("text/html; charset=utf-8"));
     }
 
-    @Test
-    public void extractExtensionメソッドのファイルから拡張子をextractするテスト() {
-        assertThat((httpResponseMessageHeaderBuilder.extractExtension(new File("hoge.html"))), is("html"));
-        assertThat((httpResponseMessageHeaderBuilder.extractExtension(new File("hoge..html"))), is("html"));
-        assertThat((httpResponseMessageHeaderBuilder.extractExtension(new File("html"))), is("html"));
+    public static class extractExtensionメソッドのテスト {
+        private File file = new File("src/test/resources/index.html");
+        HttpResponseMessageHeaderBuilder httpResponseMessageHeaderBuilder = new HttpResponseMessageHeaderBuilder(file);
+
+        @Test
+        public void ファイルから拡張子をextractするテスト() {
+            assertThat((httpResponseMessageHeaderBuilder.extractExtension(new File("hoge.html"))), is("html"));
+            assertThat((httpResponseMessageHeaderBuilder.extractExtension(new File("hoge..html"))), is("html"));
+            assertThat((httpResponseMessageHeaderBuilder.extractExtension(new File("html"))), is("html"));
+        }
     }
 
 }
