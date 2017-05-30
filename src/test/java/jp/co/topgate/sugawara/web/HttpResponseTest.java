ab06100b51a5ded2561a1153c6eca43bc327067f
sakura818 <haruka.sugawara@topgate.co.jp>
2017-05-30 14:18:26 +0900

    Add public static class createHttpResponseContentメソッドのテスト

--- src/test/java/jp/co/topgate/sugawara/web/HttpResponseTest.java
+++ src/test/java/jp/co/topgate/sugawara/web/HttpResponseTest.java
@@ -19,84 +19,87 @@ public class HttpResponseTest {
     private int statusCode;
     HttpResponse httpResponse = new HttpResponse(file, statusCode);
 
-    @Test
-    public void createHttpResponseContentメソッドのhttpResponseを適切な形で生成できているかのテスト() throws Exception {
-        assertThat(httpResponseActual(), is(httpResponseExpect()));
-    }
+    public static class createHttpResponseContentメソッドのテスト {
 
-    public byte[] httpResponseExpect() throws Exception {
+        @Test
+        public void httpResponseを適切な形で生成できているかのテスト() throws Exception {
+            assertThat(httpResponseActual(), is(httpResponseExpect()));
+        }
 
-        byte[] statusLine = "HTTP/1.1 200 OK\n".getBytes();
-        byte[] messageHeader = ("Server: sakura818\n" +
-                "Allow: GET\n" +
-                "Content-Language: en\n" +
-                "Content-Type: text/html; charset=UTF-8\n").getBytes();
-        byte[] messageBody = ("<!DOCTYPE html>\n" +
-                "<html>\n" +
-                "<head>\n" +
-                "    <meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\">\n" +
-                "    <link rel=\"stylesheet\" type=\"text/css\" href=\"./sample.css\">\n" +
-                "    <script type=\"text/javascript\" src=\"./sample.js\"></script>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "<h1>CSSでh1タグの文字を青色にします</h1>\n" +
-                "<p>JavaScriptでファイルの更新日時を表示します\n" +
-                "    <script>\n" +
-                "        lastModified_date();\n" +
-                "\n" +
-                "    </script>\n" +
-                "</p>\n" +
-                "<p>pngです<img src=\"sample.png\" width=\"100\" height=\"100\" alt=\"png\" align=\"center\"></p>\n" +
-                "<p>jpegです<img src=\"sample.jpeg\" width=\"100\" height=\"100\" alt=\"jpeg\" align=\"center\"></p>\n" +
-                "<p>gifです<img src=\"sample.gif\" width=\"100\" height=\"100\" alt=\"gif\" border=\"0\" align=\"center\" hspace=\"10\" vspace=\"10\">\n" +
-                "</p>\n" +
-                "</body>\n" +
-                "</html>").getBytes();
-        byte[] CRLF = "\r\n".getBytes("UTF-8");
-        int statusLineLength = statusLine.length;
-        int messageHeaderLength = messageHeader.length;
-        int messageBodyLength = messageBody.length;
-        int CRLFLength = CRLF.length;
+        public byte[] httpResponseExpect() throws Exception {
 
-        byte[] httpResponseExpect = new byte[statusLineLength + messageHeaderLength + messageBodyLength + CRLFLength];
+            byte[] statusLine = "HTTP/1.1 200 OK\n".getBytes();
+            byte[] messageHeader = ("Server: sakura818\n" +
+                    "Allow: GET\n" +
+                    "Content-Language: en\n" +
+                    "Content-Type: text/html; charset=UTF-8\n").getBytes();
+            byte[] messageBody = ("<!DOCTYPE html>\n" +
+                    "<html>\n" +
+                    "<head>\n" +
+                    "    <meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\">\n" +
+                    "    <link rel=\"stylesheet\" type=\"text/css\" href=\"./sample.css\">\n" +
+                    "    <script type=\"text/javascript\" src=\"./sample.js\"></script>\n" +
+                    "</head>\n" +
+                    "<body>\n" +
+                    "<h1>CSSでh1タグの文字を青色にします</h1>\n" +
+                    "<p>JavaScriptでファイルの更新日時を表示します\n" +
+                    "    <script>\n" +
+                    "        lastModified_date();\n" +
+                    "\n" +
+                    "    </script>\n" +
+                    "</p>\n" +
+                    "<p>pngです<img src=\"sample.png\" width=\"100\" height=\"100\" alt=\"png\" align=\"center\"></p>\n" +
+                    "<p>jpegです<img src=\"sample.jpeg\" width=\"100\" height=\"100\" alt=\"jpeg\" align=\"center\"></p>\n" +
+                    "<p>gifです<img src=\"sample.gif\" width=\"100\" height=\"100\" alt=\"gif\" border=\"0\" align=\"center\" hspace=\"10\" vspace=\"10\">\n" +
+                    "</p>\n" +
+                    "</body>\n" +
+                    "</html>").getBytes();
+            byte[] CRLF = "\r\n".getBytes("UTF-8");
+            int statusLineLength = statusLine.length;
+            int messageHeaderLength = messageHeader.length;
+            int messageBodyLength = messageBody.length;
+            int CRLFLength = CRLF.length;
 
-        System.arraycopy(statusLine, 0, httpResponseExpect, 0, statusLineLength);
-        System.arraycopy(messageHeader, 0, httpResponseExpect, statusLineLength, messageHeaderLength);
-        System.arraycopy(messageBody, 0, httpResponseExpect, (statusLineLength + messageHeaderLength), messageBodyLength);
-        System.arraycopy(CRLF, 0, httpResponseExpect, (statusLineLength + messageHeaderLength + messageBodyLength), CRLFLength);
+            byte[] httpResponseExpect = new byte[statusLineLength + messageHeaderLength + messageBodyLength + CRLFLength];
 
-        return httpResponseExpect;
-    }
+            System.arraycopy(statusLine, 0, httpResponseExpect, 0, statusLineLength);
+            System.arraycopy(messageHeader, 0, httpResponseExpect, statusLineLength, messageHeaderLength);
+            System.arraycopy(messageBody, 0, httpResponseExpect, (statusLineLength + messageHeaderLength), messageBodyLength);
+            System.arraycopy(CRLF, 0, httpResponseExpect, (statusLineLength + messageHeaderLength + messageBodyLength), CRLFLength);
+
+            return httpResponseExpect;
+        }
 
 
-    public byte[] httpResponseActual() throws Exception {
+        public byte[] httpResponseActual() throws Exception {
 
-        byte[] httpResponseActual = ("HTTP/1.1 200 OK\n" + "Server: sakura818\n" +
-                "Allow: GET\n" +
-                "Content-Language: en\n" +
-                "Content-Type: text/html; charset=UTF-8\n" + "<!DOCTYPE html>\n" +
-                "<html>\n" +
-                "<head>\n" +
-                "    <meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\">\n" +
-                "    <link rel=\"stylesheet\" type=\"text/css\" href=\"./sample.css\">\n" +
-                "    <script type=\"text/javascript\" src=\"./sample.js\"></script>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "<h1>CSSでh1タグの文字を青色にします</h1>\n" +
-                "<p>JavaScriptでファイルの更新日時を表示します\n" +
-                "    <script>\n" +
-                "        lastModified_date();\n" +
-                "\n" +
-                "    </script>\n" +
-                "</p>\n" +
-                "<p>pngです<img src=\"sample.png\" width=\"100\" height=\"100\" alt=\"png\" align=\"center\"></p>\n" +
-                "<p>jpegです<img src=\"sample.jpeg\" width=\"100\" height=\"100\" alt=\"jpeg\" align=\"center\"></p>\n" +
-                "<p>gifです<img src=\"sample.gif\" width=\"100\" height=\"100\" alt=\"gif\" border=\"0\" align=\"center\" hspace=\"10\" vspace=\"10\">\n" +
-                "</p>\n" +
-                "</body>\n" +
-                "</html>" + "\r\n").getBytes();
+            byte[] httpResponseActual = ("HTTP/1.1 200 OK\n" + "Server: sakura818\n" +
+                    "Allow: GET\n" +
+                    "Content-Language: en\n" +
+                    "Content-Type: text/html; charset=UTF-8\n" + "<!DOCTYPE html>\n" +
+                    "<html>\n" +
+                    "<head>\n" +
+                    "    <meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\">\n" +
+                    "    <link rel=\"stylesheet\" type=\"text/css\" href=\"./sample.css\">\n" +
+                    "    <script type=\"text/javascript\" src=\"./sample.js\"></script>\n" +
+                    "</head>\n" +
+                    "<body>\n" +
+                    "<h1>CSSでh1タグの文字を青色にします</h1>\n" +
+                    "<p>JavaScriptでファイルの更新日時を表示します\n" +
+                    "    <script>\n" +
+                    "        lastModified_date();\n" +
+                    "\n" +
+                    "    </script>\n" +
+                    "</p>\n" +
+                    "<p>pngです<img src=\"sample.png\" width=\"100\" height=\"100\" alt=\"png\" align=\"center\"></p>\n" +
+                    "<p>jpegです<img src=\"sample.jpeg\" width=\"100\" height=\"100\" alt=\"jpeg\" align=\"center\"></p>\n" +
+                    "<p>gifです<img src=\"sample.gif\" width=\"100\" height=\"100\" alt=\"gif\" border=\"0\" align=\"center\" hspace=\"10\" vspace=\"10\">\n" +
+                    "</p>\n" +
+                    "</body>\n" +
+                    "</html>" + "\r\n").getBytes();
 
-        return httpResponseActual;
+            return httpResponseActual;
+        }
     }
 
 
