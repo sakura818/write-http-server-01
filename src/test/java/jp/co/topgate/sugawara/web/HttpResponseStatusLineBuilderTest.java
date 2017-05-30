7f1cc792704ef01186b3650b5700635015d2488f
sakura818 <haruka.sugawara@topgate.co.jp>
2017-05-30 14:20:48 +0900

    Add public static class catchReasonPhraseメソッドのテスト

--- src/test/java/jp/co/topgate/sugawara/web/HttpResponseStatusLineBuilderTest.java
+++ src/test/java/jp/co/topgate/sugawara/web/HttpResponseStatusLineBuilderTest.java
@@ -30,24 +30,29 @@ public class HttpResponseStatusLineBuilderTest {
         return (statusLine.toString()).getBytes();
     }
 
-    @Test
-    public void catchReasonPhraseメソッドのstatusCodeが200のときに適切なreasonPhraseを返すかのテスト() {
-        assertThat(httpResponseStatusLineBuilder.catchReasonPhrase(200), is("OK"));
-    }
+    public static class catchReasonPhraseメソッドのテスト {
+        private int statusCode = 200;
+        HttpResponseStatusLineBuilder httpResponseStatusLineBuilder = new HttpResponseStatusLineBuilder(statusCode);
 
-    @Test
-    public void catchReasonPhraseメソッドのstatusCodeが400のときに適切なreasonPhraseを返すかのテスト() {
-        assertThat(httpResponseStatusLineBuilder.catchReasonPhrase(400), is("Bad Request"));
-    }
+        @Test
+        public void statusCodeが200のときに適切なreasonPhraseを返すかのテスト() {
+            assertThat(httpResponseStatusLineBuilder.catchReasonPhrase(200), is("OK"));
+        }
 
-    @Test
-    public void catchReasonPhraseメソッドのstatusCodeが404のときに適切なreasonPhraseを返すかのテスト() {
-        assertThat(httpResponseStatusLineBuilder.catchReasonPhrase(404), is("Not Found"));
-    }
+        @Test
+        public void statusCodeが400のときに適切なreasonPhraseを返すかのテスト() {
+            assertThat(httpResponseStatusLineBuilder.catchReasonPhrase(400), is("Bad Request"));
+        }
 
-    @Test
-    public void catchReasonPhraseメソッドのstatusCodeが500のときに適切なreasonPhraseを返すかのテスト() {
-        assertThat(httpResponseStatusLineBuilder.catchReasonPhrase(500), is("Internal Server Error"));
+        @Test
+        public void statusCodeが404のときに適切なreasonPhraseを返すかのテスト() {
+            assertThat(httpResponseStatusLineBuilder.catchReasonPhrase(404), is("Not Found"));
+        }
+
+        @Test
+        public void statusCodeが500のときに適切なreasonPhraseを返すかのテスト() {
+            assertThat(httpResponseStatusLineBuilder.catchReasonPhrase(500), is("Internal Server Error"));
+        }
     }
 
 
