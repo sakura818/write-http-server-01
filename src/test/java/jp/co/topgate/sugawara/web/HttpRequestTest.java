eb097096ef47302acf7a6e9b229c809d2f7bffd9
sakura818 <haruka.sugawara@topgate.co.jp>
2017-05-30 16:21:59 +0900

    Fix httprequest test

--- src/test/java/jp/co/topgate/sugawara/web/HttpRequestTest.java
+++ src/test/java/jp/co/topgate/sugawara/web/HttpRequestTest.java
@@ -34,23 +34,31 @@ public class HttpRequestTest {
             HttpRequest httpRequest = new HttpRequest(inputStream);
             assertThat((httpRequest.parseRequestUri("GET http://localhost:8080/index.html HTTP/1.1")), is("http://localhost:8080/index.html"));
         }
+
+        @Test
+        public void requestLineからrequestUriを抜き出すテスト3() throws IOException {
+            InputStream inputStream = new ByteArrayInputStream("GET / HTTP/1.1".getBytes("utf-8"));
+            HttpRequest httpRequest = new HttpRequest(inputStream);
+            assertThat((httpRequest.parseRequestUri("GET / HTTP/1.1")), is("/index.html"));
+        }
     }
 
     public static class parseUriPathメソッドのテスト {
 
         @Test
         public void parseUriPathメソッドのrequestUriからUriPathを抜き出すテスト1() throws IOException {
-            InputStream inputStream = new ByteArrayInputStream("GET http://localhost:8080/index.html HTTP/1.1".getBytes("utf-8"));
+            InputStream inputStream = new ByteArrayInputStream("GET /index.html HTTP/1.1".getBytes("utf-8"));
             HttpRequest httpRequest = new HttpRequest(inputStream);
-            assertThat((httpRequest.parseUriPath("http://localhost:8080/index.html")), is("index.html"));
+            assertThat((httpRequest.parseUriPath("/index.html")), is("/index.html"));
         }
 
         @Test
         public void parseUriPathメソッドのrequestUriからUriPathを抜き出すテスト2() throws IOException {
             InputStream inputStream = new ByteArrayInputStream("GET http://localhost:8080/index.html HTTP/1.1".getBytes("utf-8"));
             HttpRequest httpRequest = new HttpRequest(inputStream);
-            assertThat((httpRequest.parseUriPath("/index.html")), is("index.html"));
+            assertThat((httpRequest.parseUriPath("/index.html")), is("/index.html"));
         }
+        
     }
 }
 
