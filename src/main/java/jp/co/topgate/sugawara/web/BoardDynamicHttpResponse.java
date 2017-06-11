a312180ec56a328383271f8bb6333c09ea94bbb8
sakura818 <haruka.sugawara@topgate.co.jp>
2017-06-12 03:49:15 +0900

    BoardDynamicHttpResponseを作成

--- src/main/java/jp/co/topgate/sugawara/web/BoardDynamicHttpResponse.java
+++ src/main/java/jp/co/topgate/sugawara/web/BoardDynamicHttpResponse.java
@@ -1,7 +1,75 @@
 package jp.co.topgate.sugawara.web;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+
 /**
  * Created by haruka.sugawara on 2017/06/12.
  */
 public class BoardDynamicHttpResponse {
+
+    private File file;
+    private int statusCode;
+
+    /**
+     * StaticHttpResponseのコンストラクタ
+     *
+     * @param file,statusCode
+     */
+
+    public BoardDynamicHttpResponse(File file, int statusCode) {
+        this.file = file;
+        this.statusCode = statusCode;
+    }
+
+    /**
+     * BoardDynamicHttpResponse
+     * BoardDynamicHttpResponse= StatusLine + MessageHeader + MessageBody
+     *
+     * @param file
+     * @param statusCode
+     * @throws IOException
+     */
+
+    public byte[] createBoardDynamicHttpResponseContent(File file, int statusCode) throws IOException {
+
+        StaticHttpResponseStatusLineBuilder boardDynamicHttpResponseStatusLineBuilder = new StaticHttpResponseStatusLineBuilder(statusCode);
+        StaticHttpResponseMessageHeaderBuilder boardDynamicHttpResponseMessageHeaderBuilder = new StaticHttpResponseMessageHeaderBuilder(file);
+        StaticHttpResponseMessageBodyBuilder boardDynamicHttpResponseMessageBodyBuilder = new StaticHttpResponseMessageBodyBuilder(file);
+
+        byte[] statusLine = boardDynamicHttpResponseStatusLineBuilder.build();
+        byte[] messageHeader = boardDynamicHttpResponseMessageHeaderBuilder.build();
+        byte[] messageBody = boardDynamicHttpResponseMessageBodyBuilder.build();
+
+
+        int statusLineLength = statusLine.length;
+        int messageHeaderLength = messageHeader.length;
+        int messageBodyLength = messageBody.length;
+
+
+        byte[] createResponseContents = new byte[statusLineLength + messageHeaderLength + messageBodyLength];
+
+        System.arraycopy(statusLine, 0, createResponseContents, 0, statusLineLength);
+        System.arraycopy(messageHeader, 0, createResponseContents, statusLineLength, messageHeaderLength);
+        System.arraycopy(messageBody, 0, createResponseContents, (statusLineLength + messageHeaderLength), messageBodyLength);
+
+        return createResponseContents;
+    }
+
+
+    /**
+     * HttpResponseをOutputStreamに書き込む
+     *
+     * @param outputStream バイト出力ストリーム
+     * @throws IOException
+     */
+
+    public void writeToOutputStream(OutputStream outputStream) throws IOException {
+
+        byte[] httpResponseContent = createBoardDynamicHttpResponseContent(file, statusCode);
+        /** HttpResponseをoutputStreamに書き込む */
+        outputStream.write(httpResponseContent);
+
+    }
 }
