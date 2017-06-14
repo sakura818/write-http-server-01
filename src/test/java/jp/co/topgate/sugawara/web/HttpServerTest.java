597d6e729dfcce9e53ee32e19e1bf8592fc843ed
sakura818 <haruka.sugawara@topgate.co.jp>
2017-06-14 15:13:02 +0900

    httpservertestを修正

--- src/test/java/jp/co/topgate/sugawara/web/HttpServerTest.java
+++ src/test/java/jp/co/topgate/sugawara/web/HttpServerTest.java
@@ -15,36 +15,7 @@ import static org.junit.Assert.assertThat;
  * @author sakura818
  */
 
-@RunWith(Enclosed.class)
 public class HttpServerTest {
-
-    public static class connectメソッドのテスト {
-        final static String FILEPATH_DIR = "src/test/resources/";
-
-        @Test
-        public void UriPathとFILEPATH_DIRからfileが生成できているかのテスト() {
-            File file = new File(this.FILEPATH_DIR, "index.html");
-            System.out.println(file);
-            assertThat(file, is(new File("src/test/resources/index.html")));
-        }
-    }
-
-    public static class catchStatusCodeメソッドのテスト {
-        final static String FILEPATH_DIR = "src/test/resources/";
-        HttpServer httpServer = new HttpServer();
-
-        @Test
-        public void ファイルが存在していた場合はステータスコード200を返すかテスト() {
-            assertThat(httpServer.catchStatusCode(new File(this.FILEPATH_DIR, "index.html")), is(200));
-        }
-
-        @Test
-        public void ファイルが存在していなかった場合はステータスコード404を返すかテスト() {
-            assertThat(httpServer.catchStatusCode(new File(this.FILEPATH_DIR, "noExist.html")), is(404));
-        }
-
-    }
-
 }
 
 
