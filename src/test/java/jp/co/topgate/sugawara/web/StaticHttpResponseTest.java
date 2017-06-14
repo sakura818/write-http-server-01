f75533ca2b89249a7868c335762a24ff4bd524d7
sakura818 <haruka.sugawara@topgate.co.jp>
2017-06-14 15:19:36 +0900

    StaticHttpResponseTestの修正

--- src/test/java/jp/co/topgate/sugawara/web/StaticHttpResponseTest.java
+++ src/test/java/jp/co/topgate/sugawara/web/StaticHttpResponseTest.java
@@ -6,6 +6,9 @@ import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 
 
+import java.io.File;
+import java.io.IOException;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -18,21 +21,21 @@ import static org.junit.Assert.assertThat;
 @RunWith(Enclosed.class)
 public class StaticHttpResponseTest {
 
-    public static class createHttpResponseContentメソッドのテスト {
-
+    public static class createStaticHttpResponseContentメソッドのテスト {
         @Test
-        public void httpResponseを適切な形で生成できているかのテスト() throws Exception {
-            assertThat(httpResponseActual(), is(httpResponseExpect()));
+        public void ステータスコード200かつメッセージボディがhtmlのときレスポンスを適切なシンタックスで生成するテスト() throws IOException {
+            File file = new File("src/test/resources/index.html");
+            int statusCode = 200;
+            StaticHttpResponse httpResponse = new StaticHttpResponse(file, statusCode);
+            assertThat(httpResponse.createStaticHttpResponseContent(file, statusCode), is(httpResponseExpect()));
         }
 
-        public byte[] httpResponseExpect() throws Exception {
+        public byte[] httpResponseExpect() throws IOException {
 
-            byte[] statusLine = "HTTP/1.1 200 OK\n".getBytes();
-            byte[] messageHeader = ("Server: sakura818\n" +
+            byte[] httpResponseExpectHtml = ("HTTP/1.1 200 OK\n" + "Server: sakura818\n" +
                     "Allow: GET\n" +
                     "Content-Language: en\n" +
-                    "Content-Type: text/html; charset=UTF-8\n").getBytes();
-            byte[] messageBody = ("<!DOCTYPE html>\n" +
+                    "Content-Type: text/html; charset=UTF-8\n\n" + "<!DOCTYPE html>\n" +
                     "<html>\n" +
                     "<head>\n" +
                     "    <meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\">\n" +
@@ -44,7 +47,6 @@ public class StaticHttpResponseTest {
                     "<p>JavaScriptでファイルの更新日時を表示します\n" +
                     "    <script>\n" +
                     "        lastModified_date();\n" +
-                    "\n" +
                     "    </script>\n" +
                     "</p>\n" +
                     "<p>pngです<img src=\"sample.png\" width=\"100\" height=\"100\" alt=\"png\" align=\"center\"></p>\n" +
@@ -52,49 +54,9 @@ public class StaticHttpResponseTest {
                     "<p>gifです<img src=\"sample.gif\" width=\"100\" height=\"100\" alt=\"gif\" border=\"0\" align=\"center\" hspace=\"10\" vspace=\"10\">\n" +
                     "</p>\n" +
                     "</body>\n" +
-                    "</html>").getBytes();
-            int statusLineLength = statusLine.length;
-            int messageHeaderLength = messageHeader.length;
-            int messageBodyLength = messageBody.length;
+                    "</html>\n").getBytes();
 
-            byte[] httpResponseExpect = new byte[statusLineLength + messageHeaderLength + messageBodyLength];
-
-            System.arraycopy(statusLine, 0, httpResponseExpect, 0, statusLineLength);
-            System.arraycopy(messageHeader, 0, httpResponseExpect, statusLineLength, messageHeaderLength);
-            System.arraycopy(messageBody, 0, httpResponseExpect, (statusLineLength + messageHeaderLength), messageBodyLength);
-
-            return httpResponseExpect;
-        }
-
-
-        public byte[] httpResponseActual() throws Exception {
-
-            byte[] httpResponseActual = ("HTTP/1.1 200 OK\n" + "Server: sakura818\n" +
-                    "Allow: GET\n" +
-                    "Content-Language: en\n" +
-                    "Content-Type: text/html; charset=UTF-8\n" + "<!DOCTYPE html>\n" +
-                    "<html>\n" +
-                    "<head>\n" +
-                    "    <meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\">\n" +
-                    "    <link rel=\"stylesheet\" type=\"text/css\" href=\"./sample.css\">\n" +
-                    "    <script type=\"text/javascript\" src=\"./sample.js\"></script>\n" +
-                    "</head>\n" +
-                    "<body>\n" +
-                    "<h1>CSSでh1タグの文字を青色にします</h1>\n" +
-                    "<p>JavaScriptでファイルの更新日時を表示します\n" +
-                    "    <script>\n" +
-                    "        lastModified_date();\n" +
-                    "\n" +
-                    "    </script>\n" +
-                    "</p>\n" +
-                    "<p>pngです<img src=\"sample.png\" width=\"100\" height=\"100\" alt=\"png\" align=\"center\"></p>\n" +
-                    "<p>jpegです<img src=\"sample.jpeg\" width=\"100\" height=\"100\" alt=\"jpeg\" align=\"center\"></p>\n" +
-                    "<p>gifです<img src=\"sample.gif\" width=\"100\" height=\"100\" alt=\"gif\" border=\"0\" align=\"center\" hspace=\"10\" vspace=\"10\">\n" +
-                    "</p>\n" +
-                    "</body>\n" +
-                    "</html>").getBytes();
-
-            return httpResponseActual;
+            return httpResponseExpectHtml;
         }
     }
 
